3eea608ab42fac877962ad750ca3cc3e3c1866a6
Abubakar Javed <shykhabubakar99@gmail.com>
2024-01-09 22:08:03 +0500

    home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -9,6 +10,28 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text(
+          'Navigation Drawer',
+          style: TextStyle(color: Colors.white),
+        )),
+        backgroundColor: Color(0xff764abc),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+         Center(
+           child: TextButton(
+             onPressed: (){
+             },
+             child: const Text('Home Screen'),
+           ),
+         )
+        ],
+      ),
+    );
   }
 }
